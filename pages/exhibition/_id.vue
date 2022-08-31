2aa43b022fc52a2da29b11903d84f46c96760d43
SmaVivian <394480110@qq.com>
2022-08-31 11:53:28 +0800

    测试静态部署的详情页调用接口

--- pages/exhibition/_id.vue
+++ pages/exhibition/_id.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>详情</p>
+    <el-button @click="handleLike">测试点赞 {{name}}</el-button>
     <div class="aaa">
       <p>{{detailData.showName}}</p>
       <p>{{detailData.address}}</p>
@@ -13,6 +14,7 @@
 import axios from "axios";
 export default {
   asyncData({ params, error, $request }) {
+    console.log(222222222)
     console.log('params', params)
     let { id } = params
     return $request
@@ -20,7 +22,7 @@ export default {
       .get(`http://www.jnmuseum.com/admin/pc/esaleShow/getShowDetail.do`, {
         id: id
       }).then(res => {
-        console.log(234, res.data)
+        // console.log(234, res.data)
         return { detailData: res.data.data }
       }).catch(e => {
         error({ statusCode: 404, message: 'Post not found' })
@@ -28,16 +30,29 @@ export default {
   },
   data() {
     return {
-      detailData: {}
+      detailData: {},
+      name: ''
     }
   },
   methods: {
     getData() {
-      console.log(111)
+      // console.log(111)
+    },
+    async handleLike() {
+      // console.log(this.detailData)
+      this.$request
+        // .get(`http://www.jnmuseum.com/admin/pc/esaleShow/getShowDetail.do?id=${id}`)
+      .get(`/admin/pc/esaleShow/getShowDetail.do`, {
+        id: this.$route.params.id
+      }).then(res => {
+        console.log(11, res)
+        this.name = res.data.data.showName
+      })
     }
   },
   mounted() {
-    this.getData()
+    console.log(11111111111)
+    // this.getData()
   }
 }
 </script>
