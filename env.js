ccb7f46f108c97cf40dd38ff9192f7152c66e0e3
SmaVivian <394480110@qq.com>
2022-09-02 15:38:25 +0800

    多余的文件，生产环境配置

--- env.js
+++ env.js
@@ -8,6 +8,7 @@ module.exports = {
   // 生产环境
   pro: {
     NODE_ENV: 'production',
-    BASE_API: 'http://www.jnmuseum.com/admin' // 正式服务器地址
+    // BASE_API: 'http://www.jnmuseum.com/admin' // 正式服务器地址
+    BASE_API: 'http://localhost:8280/admin' // 正式服务器地址
   }
 }
