a62ef1d167ae1968960b65b3103b7a8e806a1357
SmaVivian <394480110@qq.com>
2022-09-02 15:41:21 +0800

    分页列表demo，列表接口采用动态获取；否则需要根据page页打包成多个列表页

---
+++ components/cmpMyPagination.vue
@@ -0,0 +1,73 @@
+<template>
+  <!-- 分页 -->
+  <div class="pagination-container">
+    <!-- layout="total, prev, pager, next, sizes" -->
+    <!-- @size-change="handleSizeChange" -->
+    <el-pagination
+      :current-page="currentPage"
+      :page-size="pageSize"
+      :total="total"
+      background
+      layout="total, prev, pager, next"
+      @current-change="handleCurrentChange"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1
+    },
+    pageSize: {
+      type: Number,
+      default: 10
+    },
+    total: {
+      type: Number,
+      default: 0
+    },
+  },
+  // watch: {
+  //   $route: {
+  //     handler(Val) {
+  //       return this.getBreadcrumb()
+  //     },
+  //     immediate: true
+  //   },
+  //   '$store.state.app.breadcumbList': function(newVal) {
+  //     if(this.$route.meta.useStore) {
+  //       this.breadList = newVal
+  //     }
+  //   }
+  // },
+  data() {
+    return {
+      // currentPage: 1
+    }
+  },
+  methods: {
+    // handleSizeChange(val) {
+    //   this.pageSize = val
+    //   this.$emit('eventPage', {
+    //     currentPage: this.currentPage,
+    //     size: this.pageSize
+    //   })
+    // },
+    handleCurrentChange(val) {
+      this.$emit('eventPage', {
+        currentPage: val,
+        size: this.pageSize
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.pagination-container {
+  
+}
+</style>
---
+++ pages/photo/index copy.vue
@@ -0,0 +1,120 @@
+<template>
+  <!-- 文创 -->
+  <div class="page-creative">
+    <div class="g-content-wrap">
+      <div class="list g-card-list">
+        <div class="item g-card-1" v-for="(item, index) in dataList" :key="index" @click="goDetail(item)">
+          <div class="pic-box">
+            <img v-lazy="item.pageUrl" alt="">
+          </div>
+          <div class="info">
+            <h2>{{item.showName}}</h2>
+            <div class="desc">
+              <p>{{item.musExhibition}}</p>
+              <p class="price"><i class="mr-5">¥</i>25</p>
+            </div>
+          </div>
+        </div>
+
+        <!-- 无数据 -->
+        <div class="g-no-data" v-if="dataList.length === 0 && !listLoading">
+          <div class="no-data-bg"></div>
+          <p class="txt text-center">暂无数据</p>
+        </div>
+      </div>
+
+      <cmpMyPagination :currentPage="listQuery.currentPage" :pageSize="listQuery.size" :total="total" @eventPage="eventPage"></cmpMyPagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ app, query, params, $request }) {
+    console.log(query)
+    console.log('params', params)
+    return app.$api.getExhibitionData({
+        // type: 1,
+        type: 2,
+        museumId: '',
+        currentPage: 1,
+        size: 6
+      }).then(res => {
+        // console.log(234, res.data)
+        return { 
+          dataList: res.data,
+          total: res.page.allRow
+        }
+      })
+  },
+  watchQuery: ['currentPage'],
+  data() {
+    return {
+      dataList: [],
+      listQuery: {
+        currentPage: 1,
+        size: 6,
+      },
+      total: 0,
+      listLoading: false,
+    }
+  },
+  methods: {
+    getDataList() {
+      // this.listLoading = true
+      // this.$http
+      //   .get('/wenChuang/getList.do', {
+      //     ...this.listQuery
+      //   })
+      //   .then(res => {
+      //     this.listLoading = false
+      //     if (res.success) {
+      //       this.dataList = res.data
+      //       this.total = res.page.allRow
+      //     } else {
+      //       this.$message.error(res.message)
+      //     }
+      //   })
+
+      this.listLoading = true
+      this.$api.getExhibitionData({
+        // type: 1,
+        type: 2,
+        museumId: '',
+        ...this.listQuery
+      }).then(res => {
+          this.listLoading = false
+          if (res.success) {
+            this.dataList = res.data
+            this.total = res.page.allRow
+          } else {
+            this.$message.error(res.message)
+          }
+        })
+    },
+    eventPage(obj) {
+      // this.listQuery.currentPage = obj.currentPage
+      // this.listQuery.size = obj.size
+      // this.getDataList()
+
+      this.$router.push({ name: 'photo', query: { currentPage: obj.currentPage } })
+    },
+    goDetail(item) {
+      this.$router.push({
+        path: '/creative/detail',
+        query: {id: item.id}
+      })
+    },
+  },
+  created() {
+    // this.getDataList()
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.page-creative {
+  
+}
+</style>
+
